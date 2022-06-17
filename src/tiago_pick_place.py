07c4cd58a9e53f06b2750f07bbb45a2d6321828e
cyberseihis <mcraftdm@gmail.com>
2022-06-17 20:03:25 +0300

    Goto fixed, pick implemented

--- src/tiago_pick_place.py
+++ src/tiago_pick_place.py
@@ -3,6 +3,7 @@ import dartpy as dp
 import RobotDART as rd
 
 from utils import create_grid, box_into_basket
+from lab_utils import AdT
 
 dt = 0.001
 simulation_time = 200.0
@@ -30,7 +31,7 @@ robot.set_positions(
 
 # Control base - we make the base fully controllable
 robot.set_actuator_type("servo", "rootJoint", False, True, False)
-robot.set_commands([0.3, 0., 0.],
+robot.set_commands([1.0, 0., 0.],
                    ['rootJoint_rot_z', 'rootJoint_pos_x', 'rootJoint_pos_y'])
 
 # Create position grid for the box/basket
@@ -40,9 +41,18 @@ basket_positions, box_positions = create_grid()
 box_size = [0.04, 0.04, 0.04]
 # Random cube position
 box_pt = np.random.choice(len(box_positions))
+box_pose = [0., 0., np.pi / 2, box_positions[box_pt][0],
+            box_positions[box_pt][1], 0.6 + (box_size[2] / 2.0)]
+box = rd.Robot.create_box(box_size, box_pose, "free", 0.1, [
+                          0.9, 0.1, 0.1, 1.0], "box_" + str(0))
+
+# Create box
+box_size = [0.04, 0.04, 0.04]
+# Random cube position
+box_pt = np.random.choice(len(box_positions))
 box_pose = [0., 0., 0., box_positions[box_pt][0],
             box_positions[box_pt][1], box_size[2] / 2.0]
-box = rd.Robot.create_box(box_size, box_pose, "free", 0.1, [
+hand_pointer = rd.Robot.create_box(box_size, box_pose, "free", 0.1, [
                           0.9, 0.1, 0.1, 1.0], "box_" + str(0))
 
 # Create basket
@@ -112,7 +122,7 @@ def angle_err():
 
 
 def stop_rot():
-    robot.set_commands([0., 0.1, 0.],
+    robot.set_commands([0., 1.0, 0.],
                        ['rootJoint_rot_z',
                         'rootJoint_pos_x',
                         'rootJoint_pos_y'])
@@ -128,6 +138,34 @@ def stop_walk():
 state = 0
 
 
+def error_task(source, target):
+    return rd.math.logMap(source.inverse().multiply(target))
+
+
+def P_vel(source, target, Kp):
+    Ad = AdT(source)
+    pvel = Ad @ error_task(source, target)
+    return pvel
+
+
+end_ef = "gripper_grasping_frame"
+mask_cmd = np.concatenate([np.zeros(17), np.ones(7), np.zeros(4)])
+
+
+def move_arm(target):
+    arm_tf = robot.body_pose(end_ef)
+    hand_rot = arm_tf.rotation()
+    target.set_rotation(hand_rot)
+    vel = P_vel(arm_tf, target, 1.)
+    print(f"vel:{vel}")
+    jac = robot.jacobian(end_ef)  # this is in world frame
+    alpha = 2.
+    cmd = alpha * (jac.T @ vel)  # using jacobian transpose
+    cmd_arm = cmd * mask_cmd
+    cmd_norm = cmd_arm / np.linalg.norm(cmd_arm, 1)
+    robot.set_commands(cmd_norm)
+
+
 def fsm():
     global state
     if state == 0:
@@ -137,10 +175,12 @@ def fsm():
             stop_rot()
     if state == 1:
         dist_r_b = bpose() - rpose()
-        dist_r_b_plane = dist_r_b[:1]
-        if np.linalg.norm(dist_r_b_plane) < 0.4:
+        if np.linalg.norm(dist_r_b) < 0.4:
             state = 2
             stop_walk()
+    if state == 2:
+        box_tf = box.base_pose()
+        move_arm(box_tf)
 
 
 for step in range(total_steps):
@@ -149,6 +189,8 @@ for step in range(total_steps):
         box_translation = box.base_pose().translation()
         basket_translation = basket.base_pose().translation()
         fsm()
+        arm_tf = robot.body_pose(end_ef)
+        hand_pointer.set_base_pose(arm_tf)
         if box_into_basket(
              box_translation, basket_translation, basket_z_angle):
             finish_counter += 1
